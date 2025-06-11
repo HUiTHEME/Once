e37140b2fddcdf28677718a773ad26f4ec656d6d
HUiTHEME <1392429292@qq.com>
2025-06-11 13:47:07 +0800

    更新1.9

---
+++ sidebar.php
@@ -0,0 +1,19 @@
+<?php if (wp_is_mobile()) { } else { ?>
+<div class="col-lg-3">
+    <div class="sidebar_sticky">
+        <?php
+        if ( is_page() ) {
+            dynamic_sidebar( 'page_widgets' );
+        }
+        else if ( is_home() || is_category() || is_tag() || is_search() ) {
+            dynamic_sidebar( 'index_widgets' );
+        }
+        else if ( is_single() ) {
+            dynamic_sidebar( 'single_widgets' );
+        }
+        else {
+        }
+        ?>
+    </div>
+</div>
+<?php } ?>
